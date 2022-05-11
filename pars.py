d62288d6d698bdac0558a2e761629683f3b8082a
Zaraza88 <87704027+Zaraza88@users.noreply.github.com>
2022-05-11 16:18:55 +0300

    Update pars.py
    Переписал в ооп

--- pars.py
+++ pars.py
@@ -1,109 +1,131 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from time import sleep
 from selenium.webdriver.common.by import By
 
 
 HOST = 'https://www.dns-shop.ru/'
 
+class SavePages(object):
+    """Класс открывает браузер, переходит на нужные страницы и сохраняет их"""
 
-def get_original_html_and_save():
-    """Получаем штмл и сохраняем его для дальнейшей обработки"""
+    def __init__(self, driver):
+        self.driver = driver
 
-    options = webdriver.ChromeOptions()
-    options.add_argument(
-        f"user-agent={'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'}"
-    )
+    #запускаем все методы класса
+    def pars(self):
+        try:
+            self.open_website()
+            self.go_to_the_right_page()
+            self.save_pages()
+        except Exception as ex:
+            print(f'---[ERROR]-{ex}-[ERROR]---')
+        finally:
+            self.driver.close()
+            self.driver.quit()
+    
+    #открываем сайт
+    def open_website(self):
+        self.driver.get('https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/')
+        self.driver.maximize_window()
+        sleep(5)
 
-    try:
-        driver = webdriver.Chrome(
-            executable_path='/home/zaraza/Projects/Парсер/parserDB/chromedriver',
-            options=options,
-        )
-        driver.get(
-            url='https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/'
-        )
-        driver.maximize_window()
-        driver.implicitly_wait(5)
+    #переходим на нужный нам город
+    def go_to_the_right_page(self):
+        self.driver.find_element(By.CLASS_NAME, 'location-icon').click()
+        sleep(2)
 
-        click_on_the_city = driver.find_element(By.CLASS_NAME, 'location-icon').click()
-        driver.implicitly_wait(2)
+        self.driver.find_element(By.CLASS_NAME, 'form-control').send_keys('Ростов-на-Дону')
+        sleep(4)
 
-        city_input = driver.find_element(By.CLASS_NAME, 'form-control')
-        driver.implicitly_wait(2)
-
-        city_input.send_keys('Ростов-на-Дону')
-        driver.implicitly_wait(4)
-
-        click_on_selected_city = driver.find_element(
+        self.driver.find_element(
             By.XPATH, '//*[@id="select-city"]/div[4]/ul[5]/li[4]/a'
         ).click()
-        driver.implicitly_wait(5)
+        sleep(5)
 
-        #сохраняем каждую страницу в отдельный файл
+    #сохраняем первые 3 html страницы 
+    def save_pages(self):
         for page in range(3, 6):
-
-            click_on_page = driver.find_element(
+            click_on_page = self.driver.find_element(
                 By.XPATH, f'//*[@id="products-list-pagination"]/ul/li[{page}]/a'
             )
             click_on_page.click()
             sleep(2)
 
             with open(f'index_page_{page}.html', 'w') as file:
-                result = file.write(driver.page_source)      
-
-    except Exception as ex:
-        print(f'[ERROR] {ex} [ERROR]')
-
-    finally:
-        driver.close()
-        driver.quit()
+                result = file.write(self.driver.page_source) 
 
 
-def parse_content_block(result):
-    """Парсим каждый блок продукта"""
+class ParseContentBlock(object):
+    """Класс собирает данные с конкретного блока и сохраняет их"""
 
-    soup = BeautifulSoup(result, 'lxml')
+    def __init__(self, result):
+        self.result = result
 
-    content_block = soup.find_all(
-        'div', class_='catalog-product ui-button-widget'
-    )
+    def parse_block(self):
+        soup = BeautifulSoup(self.result, 'lxml')
 
-    for block in content_block:
+        content_block = soup.find_all(
+            'div', class_='catalog-product ui-button-widget'
+        )
 
-        try:
-            price = block.find('div', class_='product-buy__price').text
-            price.replace('₽', '').strip().replace(' ','.')
-        except AttributeError:
-            continue
+        for block in content_block:
+            try:
+                price = block.find('div', class_='product-buy__price').text
+                price.replace('₽', '').strip().replace(' ','.')
+            except AttributeError:
+                continue
 
-        try:
-            name = block.find(
-                'a', class_='catalog-product__name ui-link ui-link_black'
-            ).text
-        except AttributeError:
-            continue
+            try:
+                name = block.find(
+                    'a', class_='catalog-product__name ui-link ui-link_black'
+                ).text
+            except AttributeError:
+                continue
+            
+            try:
+                link = HOST + block.find(
+                    'a', class_='catalog-product__name ui-link ui-link_black'
+                ).get('href')
+            except AttributeError:
+                continue
+
+            data.append([price, name, link])
+
+
+class Parsing(object):
+    """Запуск парсера"""
+
+    def init_pars(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument(
+            f"user-agent={'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36'}"
+        )
+
+        driver = webdriver.Chrome(
+            executable_path='/home/zaraza/Projects/Парсер/parserDB/chromedriver',
+            options=options
+        )
+        parser = SavePages(driver)
+        parser.pars()
+
+    #парсим сохраненный html для дальнейшего сохранения в бд
+    def pars_and_save_data(self):
+        global data
+        data = []
+        #по тз треповалось первые 30 продуктов в порядке возрастания
+        data = sorted(data[:30], reverse=True)
+
+        for page in range(3, 6):
+            with open(f"index_page_{page}.html") as file:
+                result = file.read()
+                res = ParseContentBlock(result)
+                res.parse_block()
         
-        try:
-            link = HOST + block.find(
-                'a', class_='catalog-product__name ui-link ui-link_black'
-            ).get('href')
-        except AttributeError:
-            continue
-
-        data.append([price, name, link])
+        return data
 
 
-def parse():
-
-    global data
-    data = []
-    data = sorted(data[:30], reverse=True)
-
-    get_original_html_and_save()
-
-    for page in range(3, 6):
-        with open(f"index_page_{page}.html") as file:
-            result = file.read()
-            parse_content_block(result)
-
-    return data
+def main():
+    asd = Parsing()
+    asd.init_pars()
+    return asd.pars_and_save_data()
